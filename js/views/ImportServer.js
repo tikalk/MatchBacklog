edbc2d20f4235e7a88b86ccb2bfdfa68aa732674
orizens <farhioren@gmail.com>
2013-07-29 16:35:01 +0300

    added to #14 refactor

--- js/views/ImportServer.js
+++ js/views/ImportServer.js
@@ -16,22 +16,6 @@ define([
 
 		importFromServer: function(ev){
 			this.model.fetch();
-		},
-
-		handleFileUpload: function (ev) {
-			var file = ev.target.files[0],
-				reader = new FileReader(),
-				settings = this.model.get('settings');
-			reader.onload = _.bind(function (event) {
-				var json = _(event.target.result).csvToJSON();
-				// update json with backlog_url
-				_.each(json.rows, function(row){
-					row.backlog_url = settings.get('backlog_url');
-				});
-				// update the model 
-				this.model.get('workers').reset(json.rows);
-			}, this);
-			reader.readAsText(file);
 		}
 	});
 
